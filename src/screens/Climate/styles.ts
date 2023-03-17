053e1049fd3492e7cf45e88054dfa68d9c5be174
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-17 18:15:13 -0300

    style: starting with the weather screen style

---
+++ src/screens/Climate/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const ClimateImage = styled.Image`
+  width: 100%;
+  height: 85%;
+  margin-top: -20px;
+`;
