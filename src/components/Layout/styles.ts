9a6b3dbec1dd679eb2f7fc4a5c105233d0a685c9
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-16 23:07:59 -0300

    style: creating style for layout

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  padding: 0px 22px;
+  background-color: ${({ theme }) => theme.colors.BLACK};
+`;
