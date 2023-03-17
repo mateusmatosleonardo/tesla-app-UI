d353c50ba76978c9a0c563085a38662338a8c3a7
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-17 18:10:42 -0300

    style: creating global theme for styles

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  name: "DefaultTheme",
+  colors: {
+    BLACK: "#161818",
+    WHITE: "#FAFAFA",
+    GRAY: "#EEEEEE",
+  },
+  fonts: {
+    Roboto_Regular: "Roboto_400Regular",
+  },
+};
