12697dfc587a902dd31bd1ce4558f16f2842f386
Mateus Leonardo <m4teusmatos@gmail.com>
2023-03-17 18:16:38 -0300

    chore: creating data for picklist

---
+++ src/mocks/menuOptions.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    name: "Controls",
+    icon: "car",
+  },
+  {
+    name: "Climate",
+    icon: "fan",
+  },
+  {
+    name: "Location",
+    icon: "map-marker",
+  },
+  {
+    name: "Summon",
+    icon: "ship-wheel",
+  },
+  {
+    name: "Security",
+    icon: "shield-check",
+  },
+  {
+    name: "Updates",
+    icon: "update",
+  },
+  {
+    name: "Pet mode",
+    icon: "dog",
+  },
+];
