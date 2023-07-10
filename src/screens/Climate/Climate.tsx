4cdf4f0020b419f785f1793f37940c5d27731d24
mateus <m4teusmatos@gmail.com>
2023-07-09 23:01:07 -0300

    feat: create climate screen

--- src/screens/Climate/Climate.tsx
+++ src/screens/Climate/Climate.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import * as S from './styles';
 
+import { useNavigation } from '@react-navigation/native';
+
 import Layout from '../../components/Layout/Layout';
 import climateImage from '../../assets/climate.png';
 
+import Chevron from '@expo/vector-icons/Entypo';
+import Power from '@expo/vector-icons/MaterialCommunityIcons';
+import CarDoor from '@expo/vector-icons/MaterialCommunityIcons';
+
 const ClimateScreen: React.FC = () => {
+
+  const [temperature, setTemperature] = React.useState(68);
+  const [on, setOn] = React.useState(false);
+
+  const navigation = useNavigation();
+
   return (
     <Layout>
       <S.ClimateImage source={climateImage} resizeMode="cover" />
+      <S.ButtonGoBack onPress={() => navigation.goBack()}>
+        <Chevron name='chevron-left' color="#ffffff" size={22} />
+      </S.ButtonGoBack>
+      <S.Footer>
+        <S.Label>Interior 74°F - Exterior 66°F</S.Label>
+        <S.ControlsRow>
+          <S.IconButtonContainer>
+            <Power
+              onPress={() => setOn(!on)}
+              name='power'
+              color={on ? '#ffffff' : 'gray'}
+              size={38} />
+            <S.IcontButtonText>{on ? 'On' : 'Off'}</S.IcontButtonText>
+          </S.IconButtonContainer>
+
+          <S.TemperatureContainer>
+            <Chevron
+              onPress={() => setTemperature(temperature - 1)}
+              name='chevron-left'
+              color="gray"
+              size={30}
+            />
+            <S.TemperatureText>{temperature}°</S.TemperatureText>
+            <Chevron
+              onPress={() => setTemperature(temperature + 1)}
+              name='chevron-right'
+              color="gray"
+              size={30} />
+          </S.TemperatureContainer>
+
+          <S.IconButtonContainer>
+            <CarDoor name='car-door' color="#cecece" size={38} />
+            <S.IcontButtonText>Vent</S.IcontButtonText>
+          </S.IconButtonContainer>
+        </S.ControlsRow>
+      </S.Footer>
     </Layout>
   )
 }
--- src/screens/Climate/styles.ts
+++ src/screens/Climate/styles.ts
@@ -3,6 +3,59 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 export const ClimateImage = styled.Image`
   width: 100%;
-  height: 85%;
-  margin-top: -20px;
+  height: 65%;
+`;
+
+export const ButtonGoBack = styled.Pressable`
+  width: 40px;
+  height: 40px;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50px;
+  left: 25px;
+  background-color: #2f3030;
+`;
+
+export const Footer = styled.Pressable`
+  align-items: center;
+  margin-top: auto;
+  margin-bottom: 20px;
+`;
+
+export const Label = styled.Text`
+  font-size: ${RFValue(15)}px;
+  margin: 20px 0px;
+  color: gray;
+`;
+
+export const ControlsRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const IconButtonContainer = styled.View`
+  align-items: center;
+`;
+
+export const IcontButtonText = styled.Text`
+  color: #cecece;
+  font-size: ${RFValue(16)}px;
+  font-weight: 600;
+  margin-top: 10px;
+`;
+
+export const TemperatureContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TemperatureText = styled.Text`
+  color: #ffffff;
+  font-size: ${RFValue(38)}px;
+  font-weight: 300;
+  margin: 0px 20px;
 `;
